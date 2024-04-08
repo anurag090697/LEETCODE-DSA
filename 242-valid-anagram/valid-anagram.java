e6cb14ccc7a88a17413c0d775794112a363a3725
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-04-08 12:00:05 +0530

    Time: 7 ms (37.78%) | Memory: 42.7 MB (93.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,10 +3,8 @@ class Solution {
         if(s.length() != t.length()) return false;
         int arr [] = new int[26];
         for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            arr[ch - 'a']++;
-            char ch1 = t.charAt(i);
-            arr[ch1 - 'a']--;
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
         for(int i = 0; i < 26; i++){
             if(arr[i] != 0) return false;
