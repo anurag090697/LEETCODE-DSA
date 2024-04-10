55251c2dde0e83fa68b61506614470b195d90e9a
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-04-10 23:25:20 +0530

    Time: 153 ms (78.64%) | Memory: 65.9 MB (28.30%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -6,25 +6,20 @@ var threeSum = function(nums) {
     nums.sort((a, b) => a - b);
     let ans = [];
     for(let i = 0; i < nums.length -2; i++){
-        if (i === 0 || nums[i] !== nums[i - 1]) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
             let left = i + 1, right = nums.length - 1;
-
             while (left < right) {
                 const sum = nums[i] + nums[left] + nums[right];
-
                 if (sum === 0) {
                     ans.push([nums[i], nums[left], nums[right]]);
                     while (nums[left] === nums[left + 1]) left++;
                     while (nums[right] === nums[right - 1]) right--;
                     left++;
                     right--;
-                } else if (sum > 0) {
-                    right--;
-                } else {
-                    left++;
                 }
-            }
-        }
+                else if (sum > 0) right--;
+                else left++;
+            }    
     }
     return ans;
 };
