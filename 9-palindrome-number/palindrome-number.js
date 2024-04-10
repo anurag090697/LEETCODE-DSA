7159253fa41338b66cf01adf72ede8988d340e63
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-04-10 11:59:32 +0530

    Time: 152 ms (17.75%) | Memory: 56.6 MB (89.41%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -4,7 +4,12 @@
  */
 var isPalindrome = function(x) {
     let str = "" + x;
-    let rev = str.split('').reverse().join('');
-    console.log(str, x);
-    return str == rev;
+    let i = 0;
+    let j = str.length -1 ;
+    while(i <= j){
+        if(str.charAt(i) !== str.charAt(j)) return false;
+        i++;
+        j--;
+    }
+    return true;
 };
