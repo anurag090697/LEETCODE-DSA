e65bb1943fca94515223bc4d72743c6c02a4b8c6
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-04-11 21:52:14 +0530

    Time: 69 ms (48.34%) | Memory: 57.1 MB (48.07%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i = 0;
+    let j = height.length-1;
+    let ans = -Infinity;
+    while(i < j){
+        let tm = Math.min(height[i], height[j]) * (j - i);
+        ans = Math.max(tm, ans);
+        if (height[i] < height[j]) {
+        i++;
+        } else {
+        j--;
+        }
+    }
+    return ans;
+};
