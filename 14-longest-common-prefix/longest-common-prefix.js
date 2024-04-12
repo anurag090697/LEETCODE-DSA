9ce4a3b7a90b2cde4b6dcdd04d1943eee5905559
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-04-12 13:38:03 +0530

    Time: 47 ms (91.03%) | Memory: 50.4 MB (30.08%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -2,21 +2,20 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(a) {
+var longestCommonPrefix = function (a) {
     let n = a.length;
-        let m = a[0];
-        
-        let cnt = 300;
-        for(let i = 0; i < n; i++){
+    let m = a[0];
+    let cnt = 300;
+    for (let i = 0; i < n; i++) {
         let idx = 0;
-            let tm = a[i];
-            while(idx < tm.length && idx < cnt){
-                if(m.charAt(idx) == tm.charAt(idx)){
-                    idx++;
-                }
-                else break;
+        let tm = a[i];
+        while (idx < tm.length && idx < cnt) {
+            if (m.charAt(idx) == tm.charAt(idx)) {
+                idx++;
             }
-            cnt = Math.min(idx, cnt);
+            else break;
         }
-        return (m.substring(0, cnt));
+        cnt = Math.min(idx, cnt);
+    }
+    return m.substring(0, cnt);
 };
