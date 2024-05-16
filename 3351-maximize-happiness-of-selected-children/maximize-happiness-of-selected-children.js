37910e7ea0fee748fd2383f17597429acc989d58
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-05-16 17:58:28 +0530

    Time: 220 ms (97.24%) | Memory: 66.6 MB (97.35%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var maximumHappinessSum = function (happiness, k) {
-    happiness.sort((a,b) => a-b);
+    happiness.sort((a,b) => b-a);
     let ans = 0, diff = 0;
-    let i = happiness.length - 1;
+    let i = 0;
 
     while (k > 0) {
         let tm = happiness[i] - diff;
         if (tm > 0) ans += tm;
         diff++;
         k--;
-        i--;
+        i++;
     }
     return ans;
 };
