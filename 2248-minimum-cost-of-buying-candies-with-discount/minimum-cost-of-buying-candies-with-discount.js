c25836bf004553bfe54b2e6891f768240d517122
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-05-29 17:04:35 +0530

    Time: 36 ms (100.00%) | Memory: 51 MB (19.05%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.js
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.js
@@ -7,13 +7,13 @@ var minimumCost = function (cost) {
     cost.sort(function(a,b){return a-b});
     let i = n - 1, ans = 0, cnt = 0;
     while (i > -1) {
-        if (cnt < 2) {
-            ans += cost[i];
-            cnt++;
-        }
-        else {
+        if (cnt === 2) {
             cnt = 0;
+            i--;
+            continue;
         }
+        ans += cost[i];
+        cnt++;
         i--;
     }
     return ans;
