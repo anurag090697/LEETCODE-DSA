db01606ac84cc5565e87a1f5197f3c62d17c7fe7
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-08-13 12:14:21 +0530

    Time: 51 ms (15.05%) | Memory: 45.7 MB (29.25%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,22 +1,23 @@
 class Solution {
     public String frequencySort(String str) {
-        Character arr[] = new Character[str.length()];
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = str.charAt(i);
-            hm.put(str.charAt(i), hm.getOrDefault(str.charAt(i), 0) + 1);
+        HashMap<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
         }
-        Comparator<Character> myComp = new Comparator<>() {
-            public int compare(Character a, Character b) {
-                if (hm.get(a).equals(hm.get(b))) {
-                    return a - b;
-                }
-                return hm.get(b) - hm.get(a);
+        List<Character> characters = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            characters.add(c);
+        }
+        characters.sort((a, b) -> {
+            int freqCompare = frequencyMap.get(b).compareTo(frequencyMap.get(a));
+            if (freqCompare != 0) {
+                return freqCompare;
+            } else {
+                return Character.compare(a, b);
             }
-        };
-        Arrays.sort(arr, myComp);
-        StringBuilder sb = new StringBuilder(arr.length);
-        for (Character c : arr) {
+        });
+        StringBuilder sb = new StringBuilder(str.length());
+        for (char c : characters) {
             sb.append(c);
         }
         return sb.toString();
