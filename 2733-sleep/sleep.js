5f4aea39065676aa95d08d9a717c025e971a4d4d
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-08-14 19:53:08 +0530

    Time: 54 ms (47.38%) | Memory: 49.1 MB (14.72%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -3,7 +3,7 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    return new Promise((resolve) => {
+    await new Promise((resolve) => {
         setTimeout(resolve, millis)
     })
 }
