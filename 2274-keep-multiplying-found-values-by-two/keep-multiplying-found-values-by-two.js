541b691554b471166ed316df6e4ba36ec0739e9f
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-08-25 13:37:53 +0530

    Time: 51 ms (70.45%) | Memory: 48.7 MB (93.13%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
@@ -4,10 +4,6 @@
  * @return {number}
  */
 var findFinalValue = function(nums, original) {
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] == original){
-            return findFinalValue(nums, original*2);
-        }
-       }
-       return original;
+    while(nums.includes(original)) original *= 2;
+    return original;
 };
