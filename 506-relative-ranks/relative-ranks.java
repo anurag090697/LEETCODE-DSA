9a48a742f92b80c3296e1475c25aed343504fce4
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-08-31 02:08:43 +0530

    Time: 11 ms (28.47%) | Memory: 45.6 MB (71.89%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,31 +1,28 @@
 class Solution {
     public String[] findRelativeRanks(int[] arr) {
         int n = arr.length;
-        HashMap<Integer, String> hm = new HashMap<>();
+        HashMap<Integer,String> hm = new HashMap<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i = 0; i < n; i++) pq.add(arr[i]);
-        int tm = pq.remove();
-        hm.put(tm, "Gold Medal");
-
-        while(!pq.isEmpty()){
-            if(pq.size() == n-1){
-                tm = pq.remove();
-                hm.put(tm, "Silver Medal");
+        for(int i = 0; i < n; i++){
+            pq.add(arr[i]);
+        }
+        for(int i = 0; i < n; i++){
+            
+            if(pq.size() == n){
+                hm.put(pq.remove(), "Gold Medal");
+            }
+            else if(pq.size() == n-1){
+                hm.put(pq.remove(), "Silver Medal");
             }
             else if(pq.size() == n-2){
-                tm = pq.remove();
-                hm.put(tm, "Bronze Medal");
+                hm.put(pq.remove(), "Bronze Medal");
             }
             else{
-                tm = pq.remove();
-                String l = "" + (hm.size()+1);
-                hm.put(tm, l);
+                hm.put(pq.remove(), (n-pq.size()) +"");
             }
         }
-        String ans [] = new String[n];
-        for(int i = 0; i < n; i++){
-            ans[i] = hm.get(arr[i]);
-        }
-        return ans;
+        String ans[] = new String[n];
+        for(int i = 0; i < n; i++) ans[i] = hm.get(arr[i]);
+        return ans; 
     }
 }
