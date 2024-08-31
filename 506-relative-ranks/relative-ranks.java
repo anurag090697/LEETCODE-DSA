714720457d37e88b08305d71e6b5f9eb81518a33
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-08-31 12:43:30 +0530

    Time: 8 ms (73.05%) | Memory: 45.8 MB (22.21%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -2,27 +2,29 @@ class Solution {
     public String[] findRelativeRanks(int[] arr) {
         int n = arr.length;
         HashMap<Integer,String> hm = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i = 0; i < n; i++){
-            pq.add(arr[i]);
+        int nums [] = new int[n];
+        
+        for(int i = n-1; i > -1; i--){
+           nums[i] = arr[i];
         }
-        for(int i = 0; i < n; i++){
+        Arrays.sort(arr);
+        for(int i = n-1; i > -1; i--){
             
-            if(pq.size() == n){
-                hm.put(pq.remove(), "Gold Medal");
+            if(i == n-1){
+                hm.put(arr[i], "Gold Medal");
             }
-            else if(pq.size() == n-1){
-                hm.put(pq.remove(), "Silver Medal");
+            else if(i == n-2){
+                hm.put(arr[i], "Silver Medal");
             }
-            else if(pq.size() == n-2){
-                hm.put(pq.remove(), "Bronze Medal");
+            else if(i == n-3){
+                hm.put(arr[i], "Bronze Medal");
             }
             else{
-                hm.put(pq.remove(), (n-pq.size()) +"");
+                hm.put(arr[i], (n-i) +"");
             }
         }
         String ans[] = new String[n];
-        for(int i = 0; i < n; i++) ans[i] = hm.get(arr[i]);
+        for(int i = 0; i < n; i++) ans[i] = hm.get(nums[i]);
         return ans; 
     }
 }
