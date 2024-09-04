cced13226f6c6fee626c86bd0fbb0a3075cfd122
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-09-04 22:02:34 +0530

    Time: 51 ms (97.65%) | Memory: 51.6 MB (65.46%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.js
+++ 1168-duplicate-zeros/duplicate-zeros.js
@@ -3,14 +3,14 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function (arr) {
+    let ans = [];
     for(let i = 0; i < arr.length; i++){
+        ans.push(arr[i]);
         if(arr[i] === 0){
-            let j = arr.length-1;
-            while(j > i){
-                arr[j] = arr[j-1];
-                j--;
-            }
-            i++;
+            ans.push(0);
         }
+    }
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = ans[i];
     }
 };
