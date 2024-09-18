cb5aa0ebcbba0d6241d835073a37bb1bd096f054
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-09-19 00:20:57 +0530

    Time: 1 ms (98.90%) | Memory: 41 MB (82.05%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.java
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.java
@@ -1,18 +1,14 @@
 class Solution {
     public int generateKey(int num1, int num2, int num3) {
-        StringBuilder ans = new StringBuilder("");
-        String s1 = num1 + "", s2 = num2 + "", s3 = num3 + "";
-        int i = s1.length()-1, j = s2.length()-1, k = s3.length()-1;
-        
-        while (i > -1 && j > -1 && k > -1) {
-            char t1 = s1.charAt(i), t2 = s2.charAt(j), t3 = s3.charAt(k);
-            char tm = (char) Math.min(t1, Math.min(t2, t3));
-            ans.append(tm);
-            i--;
-            j--;
-            k--;
+        int ans = 0 , n = 1;
+        for(int i = 0; i < 4; i++){
+            int tm = Math.min(num1 % 10, Math.min(num2 % 10, num3 % 10));
+            ans += n * tm;
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+            n *= 10;
         }
-        String a = ans.reverse().toString();
-        return Integer.parseInt(a);
+        return ans;
     }
 }
