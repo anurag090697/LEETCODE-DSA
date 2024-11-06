c70496eb9e24530d10382896f59a2590601ccdc6
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-11-06 19:42:37 +0530

    Time: 4 ms (99.64%) | Memory: 45.5 MB (28.85%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,23 +1,18 @@
 class Solution {
-    public boolean validPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-            }
-            left++;
-            right--;
+    public boolean isPalindrome(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i++) != s.charAt(j--)) return false;
         }
-        
         return true;
-    }    
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+    }
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length()-1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
             }
-            left++;
-            right--;
+            i++;
+            j--;
         }
         return true;
     }
