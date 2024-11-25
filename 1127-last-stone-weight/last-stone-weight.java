8dcab3c294b74f9c1b76128b77e62cce3707f058
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-11-26 01:16:47 +0530

    Time: 1 ms (98.99%) | Memory: 41 MB (46.21%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -3,8 +3,8 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i : arr) pq.add(i);
         while(pq.size() > 1){
-            int stone1 = pq.remove(), stone2 = pq.remove();
-            if(stone1 - stone2 != 0) pq.add(stone1-stone2);
+            int tm = pq.remove() - pq.remove();
+            if(tm != 0) pq.add(tm);
         }
         return pq.isEmpty() ? 0 : pq.remove();
     }
