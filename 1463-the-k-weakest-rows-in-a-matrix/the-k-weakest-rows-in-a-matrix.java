3165d863a1d4ba713afe45aa375bbecec094514f
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-11-25 12:32:35 +0530

    Time: 2 ms (79.94%) | Memory: 45 MB (22.22%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -4,13 +4,11 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i = 0; i < n; i++){
             int sum = 0;
-            for(int j = 0; j < m; j++){
-                sum += mat[i][j];
-            }
-            pq.add((sum * 10000) + i);
+            for(int j = 0; j < m; j++) sum += mat[i][j];
+            pq.add((sum*10000)+i);
         }
-        int ans[] = new int[k];
-        for(int i = 0; i < ans.length; i++) ans[i] = pq.remove() % 10000;
+        int ans[] = new int [k];
+        for(int i = 0; i < k; i++) ans[i] = pq.remove() % 10000;
         return ans;
     }
 }
