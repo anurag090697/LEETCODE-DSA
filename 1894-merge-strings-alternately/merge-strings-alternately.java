1076cf77fe9f7c82dfb62200468906009ce67979
Anurag Shukla <147614514+anurag090697@users.noreply.github.com>
2024-12-01 01:55:27 +0530

    Time: 7 ms (16.77%) | Memory: 42.9 MB (9.99%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,13 +1,17 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int i = 0;
+        int i = 0, j = 0;
+        char arr[] = new char[word1.length() + word2.length()];
         String ans = "";
+        // System.out.print(arr.toString());
         while(i < word1.length() && i < word2.length()){
-            ans += word1.charAt(i);
-            ans += word2.charAt(i++);
+            arr[j++] = word1.charAt(i);
+            arr[j++] = word2.charAt(i++);
         }
-        while(i < word1.length()) ans += word1.charAt(i++);
-        while(i < word2.length()) ans += word2.charAt(i++);
+        
+        while(i < word1.length()) arr[j++] = word1.charAt(i++);
+        while(i < word2.length()) arr[j++] = word2.charAt(i++);
+        for(char p : arr) ans += p;
         return ans;
     }
 }
